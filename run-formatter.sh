d10314ab4f01c7351e9c75eda0a0970bd83e78c7
Magdalena Andrys <mandrys@antmicro.com>
2021-05-18 13:04:40 +0200

    generate annotations based on a formatter output

---
+++ run-formatter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILE_IN=./hello/hello.c
+FILE_OUT=./hello/hello-formatted.c
+DIFF_LOG=$FILE_IN-diff.log
+
+clang-format $FILE_IN > $FILE_OUT
+diff -u $FILE_IN $FILE_OUT > $DIFF_LOG
+echo $DIFF_LOG
+
+#TODO: split diff file into smaller chunks
