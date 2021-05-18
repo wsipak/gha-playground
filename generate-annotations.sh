d288e5ee000e04826c48b7fe8b11a0fa55d8bf03
Magdalena Andrys <mandrys@antmicro.com>
2021-05-18 22:10:25 +0200

    generate annotations directly from a script

---
+++ generate-annotations.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+get_format_diff () {
+	clang-format $1 > $2
+	diff -u $1 $2 > $3
+}
+
+DIR="./hello"
+
+for file in $DIR/*.c; do
+
+	echo $file
+	FILE_IN=$file
+	FILE_OUT=$file-formatted.c
+	DIFF_LOG=$FILE_IN-diff.log
+
+	get_format_diff $FILE_IN $FILE_OUT $DIFF_LOG
+
+	DIFF_TEXT=`cat $DIFF_LOG`
+	DIFF_TEXT="${DIFF_TEXT//'%'/'%25'}"
+	DIFF_TEXT="${DIFF_TEXT//$'\n'/'%0A'}"
+	DIFF_TEXT="${DIFF_TEXT//$'\r'/'%0D'}"
+	LINE=`cut -sd "@" -f3 $DIFF_LOG | cut -d "," -f2 | cut -f1 -d " "`
+	echo "::error file=$file,line=$LINE,col=1::$DIFF_TEXT"
+
+done
+
+#TODO: split diff files into smaller chunks
