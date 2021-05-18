1c2ef865772a475601414e26e6ccce8306cd1df3
Magdalena Andrys <mandrys@antmicro.com>
2021-05-18 22:17:56 +0200

    echo variables

--- generate-annotations.sh
+++ generate-annotations.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 get_format_diff () {
+	echo $1 $2 $3
 	clang-format $1 > $2
 	diff -u $1 $2 > $3
 }
@@ -17,6 +18,7 @@ for file in $DIR/*.c; do
 	get_format_diff $FILE_IN $FILE_OUT $DIFF_LOG
 
 	DIFF_TEXT=`cat $DIFF_LOG`
+	echo $DIFF_TEXT
 	DIFF_TEXT="${DIFF_TEXT//'%'/'%25'}"
 	DIFF_TEXT="${DIFF_TEXT//$'\n'/'%0A'}"
 	DIFF_TEXT="${DIFF_TEXT//$'\r'/'%0D'}"
